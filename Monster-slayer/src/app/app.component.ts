7039b8568c76de0c56203def5fe82b871e41df51
KERNGRUPPE\nk.azahus87 <azadar.hussain@netzkern.de>
2018-03-16 11:05:11 +0100

    First step

--- Monster-slayer/src/app/app.component.ts
+++ Monster-slayer/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  playerHealth = 100;
+  monsterHealth = 100;
+  gameIsRunning = false;
+  turns = [];
 }
